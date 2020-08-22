ffa44466004a4ae60905bd80a90050afa4398b17
kiran-shetkar <kiran.shetkar01@gmail.com>
2020-08-22 16:16:48 +0530

    Initial commit

---
+++ src/main/java/com/my/university/UniversityApplication.java
@@ -0,0 +1,14 @@
+package com.my.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//https://www.javaguides.net/2018/09/spring-boot-2-jersey-rest-jpa-hibernate-5-crud-rest-apis-example.html
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//https://www.baeldung.com/spring-boot-failed-to-configure-data-source
+public class UniversityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityApplication.class, args);
+    }
+}
